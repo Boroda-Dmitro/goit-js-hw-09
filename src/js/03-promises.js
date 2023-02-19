ebe3473c37114d8c52f56823e667839915523ebd
Boroda-Dmitro <112397651+Boroda-Dmitro@users.noreply.github.com>
2023-02-19 17:45:06 +0200

    fix 3 hw

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,49 +10,99 @@ const onSubmitCreatePromise = event => {
   const {
     elements: { delay, step, amount },
   } = event.currentTarget;
-
   let promisDelay = Number(delay.value) + Number(step.value);
 
-  setTimeout(() => {
-    createPromise(promisNumber, delay.value)
+  createPromise(promisNumber, delay.value)
+    .then(value => {
+      Notiflix.Notify.success(value);
+    })
+    .catch(value => {
+      Notiflix.Notify.failure(value);
+    });
+
+  timerId = setInterval(() => {
+    promisNumber += 1;
+    createPromise(promisNumber, promisDelay)
       .then(value => {
         Notiflix.Notify.success(value);
       })
       .catch(value => {
         Notiflix.Notify.failure(value);
-      })
-      .finally(() => {
-        return (promisNumber += 1);
       });
-
-    timerId = setInterval(() => {
-      createPromise(promisNumber, promisDelay)
-        .then(value => {
-          Notiflix.Notify.success(value);
-        })
-        .catch(value => {
-          Notiflix.Notify.failure(value);
-        });
-      promisNumber += 1;
-      promisDelay += Number(step.value);
-      if (promisNumber > Number(amount.value)) {
-        clearInterval(timerId);
-        promisDelay = 0;
-        promisNumber = 1;
-      }
-    }, step.value);
-  }, delay.value);
+  
+    promisDelay += Number(step.value);
+    if (promisNumber === Number(amount.value)) {
+      clearInterval(timerId);
+      promisDelay = 0;
+      promisNumber = 1;
+    }
+  }, step.value);
 };
+
+form.addEventListener('submit', onSubmitCreatePromise);
 
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
-    if (shouldResolve) {
-      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    } else {
-      reject(`❌ Rejected promise ${position} in ${delay}ms`);
-    }
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
   });
 }
 
-form.addEventListener('submit', onSubmitCreatePromise);
+
+// альтернативний варіант
+// const onSubmitCreatePromise = event => {
+//   event.preventDefault();
+
+// const {
+//   elements: { delay, step, amount },
+// } = event.currentTarget;
+
+//   let promisDelay = Number(delay.value) + Number(step.value);
+
+//   setTimeout(() => {
+// createPromise(promisNumber, delay.value)
+//   .then(value => {
+//     Notiflix.Notify.success(value);
+//   })
+//   .catch(value => {
+//     Notiflix.Notify.failure(value);
+//   })
+//   .finally(() => {
+//     return (promisNumber += 1);
+//   });
+
+// timerId = setInterval(() => {
+//   createPromise(promisNumber, promisDelay)
+//     .then(value => {
+//       Notiflix.Notify.success(value);
+//     })
+//     .catch(value => {
+//       Notiflix.Notify.failure(value);
+//     });
+//   promisNumber += 1;
+//   promisDelay += Number(step.value);
+//   if (promisNumber > Number(amount.value)) {
+//     clearInterval(timerId);
+//     promisDelay = 0;
+//     promisNumber = 1;
+//   }
+// }, step.value);
+//   }, delay.value);
+// };
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     if (shouldResolve) {
+//       resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//     } else {
+//       reject(`❌ Rejected promise ${position} in ${delay}ms`);
+//     }
+//   });
+// }
